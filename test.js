c471bfe2f422dcce6b56eed4124db0ee37a2208a
nurtakin <101595387+nurtakin@users.noreply.github.com>
2022-10-20 00:57:48 +0400

    Add files via upload

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
     const numbers = [1,2,4,6,71,2,4,9,100]
-    const min= Math.min(...numbers);
-    const max= Math.max(...numbers);
+    const min= Math.min.apply(null,numbers);
+    const max= Math.max.apply(null,numbers);
 
 
     console.log(`min is : ${min}`);
